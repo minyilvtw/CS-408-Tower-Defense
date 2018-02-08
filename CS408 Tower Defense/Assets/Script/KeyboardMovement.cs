1abe72eab27a2628054b405a0510d82fd45a2efc
Tiger Lu <tigerlu330@hotmail.com>
2018-02-08 17:13:15 -0500

    Update Movement / Look at mouse
    movement actually works

---
+++ CS408 Tower Defense/Assets/Script/KeyboardMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class KeyboardMovement : MonoBehaviour
+{
+    public float mSpeed = 10;
+    void Update()
+    {
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * mSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * mSpeed;
+        
+        Vector3 position = this.transform.position;
+        position.x += x;
+        position.z += z;
+        this.transform.position = position;
+    }
+
+
+    float force = 3;
+
+    void OnCollisionEnter(Collision c)
+    {
+        //var hit = collision.gameObject;
+        // var health = hit.GetComponent<playerStatus>();
+        //if (health != null) health.TakeDamage(10);
+
+        if (c.gameObject.tag == "Enemy")
+        {
+            // Calculate Angle Between the collision point and the player
+            Vector3 dir = c.contacts[0].point - transform.position;
+            // We then get the opposite (-Vector3) and normalize it
+            dir = -dir.normalized;
+            // And finally we add force in the direction of dir and multiply it by force. 
+            // This will push back the player
+            GetComponent<Rigidbody>().AddForce(dir * force);
+        }
+
+        //Destroy(gameObject);
+
+    }
+
+}
