4dfe52a0ea1a7eba718684158a7d909139a16130
Tiger Lu <tigerlu330@hotmail.com>
2018-02-09 02:20:47 -0500

    Fixed movement / camera follow

--- CS408 Tower Defense/Assets/Script/KeyboardMovement.cs
+++ CS408 Tower Defense/Assets/Script/KeyboardMovement.cs
@@ -15,28 +15,4 @@ public class KeyboardMovement : MonoBehaviour
         this.transform.position = position;
     }
 
-
-    float force = 3;
-
-    void OnCollisionEnter(Collision c)
-    {
-        //var hit = collision.gameObject;
-        // var health = hit.GetComponent<playerStatus>();
-        //if (health != null) health.TakeDamage(10);
-
-        if (c.gameObject.tag == "Enemy")
-        {
-            // Calculate Angle Between the collision point and the player
-            Vector3 dir = c.contacts[0].point - transform.position;
-            // We then get the opposite (-Vector3) and normalize it
-            dir = -dir.normalized;
-            // And finally we add force in the direction of dir and multiply it by force. 
-            // This will push back the player
-            GetComponent<Rigidbody>().AddForce(dir * force);
-        }
-
-        //Destroy(gameObject);
-
-    }
-
 }
