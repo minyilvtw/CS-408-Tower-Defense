db927d0bb3c99eb580f4057f3db5bbf902c09efe
Tiger Lu <tigerlu330@hotmail.com>
2018-02-11 20:46:44 -0500

    Game Prototype ready

--- CS408 Tower Defense/Assets/Script/Menu/PauseMenu.cs
+++ CS408 Tower Defense/Assets/Script/Menu/PauseMenu.cs
@@ -9,6 +9,10 @@ public class PauseMenu : MonoBehaviour {
 
     public GameObject pauseMenuUI;
 
+    private void Start()
+    {
+        Resume();
+    }
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.Escape))
