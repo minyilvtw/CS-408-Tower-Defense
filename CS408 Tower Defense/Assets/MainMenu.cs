a2305060304153c637e22759378c9d10b1cd1beb
Tiger Lu <tigerlu330@hotmail.com>
2018-02-11 20:15:15 -0500

    text mesh works and everything

--- CS408 Tower Defense/Assets/MainMenu.cs
+++ CS408 Tower Defense/Assets/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour {
 	public void PlayGame()
     {
         Debug.Log("Loading next scene");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 
     }
     public void QuitGame()
