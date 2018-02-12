5fabe5f22bbf15d5673129b617a3e65ca1825d23
Tiger Lu <tigerlu330@hotmail.com>
2018-02-11 20:16:19 -0500

    change build main

--- CS408 Tower Defense/Assets/MainMenu.cs
+++ CS408 Tower Defense/Assets/MainMenu.cs
@@ -8,8 +8,8 @@ public class MainMenu : MonoBehaviour {
 	public void PlayGame()
     {
         Debug.Log("Loading next scene");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Main");
     }
     public void QuitGame()
     {
