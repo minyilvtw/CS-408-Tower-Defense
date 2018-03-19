098bf7528e865603941640435f9673d7c307f660
Tiger Lu <tigerlu330@hotmail.com>
2018-03-18 21:48:04 -0400

    health

--- CS408 Tower Defense/Assets/Script/Manager/LevelManager.cs
+++ CS408 Tower Defense/Assets/Script/Manager/LevelManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class LevelManager : MonoSingleton<LevelManager> {
 
+    private int lifePoint = 25;
     private int gold = 100;
     private int currentLevel;
     private int currentWave;
